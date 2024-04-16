39e1ec2abf5fd123d584ab02b143161a97b18ac1
just_a_mango <horace.hoff@gmail.com>
2024-04-16 02:43:35 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
-
-
-  return (
-    <div className="App">
-    <div className="veber">
-      <div className="title-money">💸</div>
-      <h1 className="vebertitle">VEBER</h1>
-      <div className="title-money-bis">💸</div>
-    </div>
-    <h3 className="catchphrase">Rust server that allows you to create your own economy</h3>
-    </div>
-  )
+    return (
+        <>
+            <div className="veber">
+                <div className="title-money">💸</div>
+                <h1 className="vebertitle">VEBER</h1>
+                <div className="title-money-bis">💸</div>
+            </div>
+            <h2 className="catchphrase">Rust server that allows you to create your own economy</h2>
+            <div className="veber-links">
+                <button className="veber-download">
+                    DOWNLOAD
+                </button>
+                <a className="veber-github" href="https://github.com/just-a-mango/veber" target="_blank">
+                    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23">
+                        <g clipPath="url(#clip0_489_191354)">
+                            <path
+                                d="M9.462 18.885C4.846 20.168 4.846 16.32 3 16.32M15.923 21v-3.331c.04-.463-.048-.931-.243-1.356-.178-.387.028-.949.447-1.025C18.658 14.832 21 13.545 21 9.452c0-.976-.331-1.92-.938-2.706a1.381 1.381 0 0 1-.248-1.14 4.028 4.028 0 0 0-.11-2.152c-.094-.28-.34-.47-.634-.445-.494.043-1.4.255-2.783 1.06-.234.136-.512.18-.776.123a13.31 13.31 0 0 0-5.637 0 1.088 1.088 0 0 1-.777-.123c-1.383-.805-2.288-1.017-2.783-1.06-.293-.025-.54.166-.633.445a4.028 4.028 0 0 0-.11 2.152c.087.396 0 .82-.248 1.14a4.42 4.42 0 0 0-.938 2.732c0 4.073 2.353 5.352 4.891 5.828.416.079.615.633.436 1.017a2.708 2.708 0 0 0-.25 1.346V21"
+                                stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                strokeLinejoin="round"></path>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_489_191354">
+                                <path fill="currentColor" d="M0 0H24V24H0z"></path>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                    VIEW ON GITHUB
+                </a>
+            </div>
+        </>
+    )
 }
 
 export default App
